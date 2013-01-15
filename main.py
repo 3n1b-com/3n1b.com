c74c988b0b4e3cbabeb5a4beb672763190fe1ede
gaolinjie <gaolinjie@gmail.com>
2013-01-15 12:21:02 +0800

    add initial files

---
+++ main.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+from tornado.options import define, options, logging
+import tornado.web
+
+define("port", default=8888, help="run on the given port", type=int)
+
+settings = {
+    "debug": True,
+}
+
+server_settings = {
+    "xheaders" : True,
+}
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, Linode")
+
+def main():
+    tornado.options.parse_command_line()
+    logging.info("Starting Tornado web server on http://localhost:%s" % options.port)
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ], **settings)
+    application.listen(options.port, **server_settings)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
