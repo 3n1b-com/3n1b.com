3fd43449731ff9a8690df96c277f9a925b072c7d
gaolinjie <gaolinjie@gmail.com>
2013-02-17 13:50:57 +0800

    add orca.txt

--- application.py
+++ application.py
@@ -92,6 +92,7 @@ class Application(tornado.web.Application):
             (r"/(favicon\.ico)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
             (r"/(sitemap.*$)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
             (r"/(bdsitemap\.txt)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
+            (r"/(orca\.txt)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
             (r"/(.*)", handler.topic.ProfileHandler),
         ]
 
